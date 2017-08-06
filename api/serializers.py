b4943a14cb0fb1f5d96049c073fdf90ae040248e
Hakim Boulahya <hakimblhya@gmail.com>
2017-08-06 19:05:14 +0200

    [api] TaskSerializer extend ModelSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -3,14 +3,7 @@ from rest_framework import serializers
 from api.models import Task
 
 
-class TaskSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=True, allow_blank=False, max_length=100)
-
-    def create(self, validated_data):
-        return Task.objects.create(validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.save()
-        return instance
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ('id', 'title')
