7c110754fd527249fc26dbb5d1458162402e185f
Hakim Boulahya <hakimblhya@gmail.com>
2017-08-09 01:45:36 +0200

    [api][models] updating according to MODEL Reference

--- api/models.py
+++ api/models.py
@@ -1,23 +1,32 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 
 class FergoUser(AbstractUser):
     pass
 
 
-class UserProfile():
+class UserProfile(models.Model):
     user = models.OneToOneField(FergoUser, on_delete=models.CASCADE)
 
 
 class Project(models.Model):
     name = models.CharField(max_length=settings.PROJECT_SETTINGS['name_max_length'], blank=False)
-    # NOTE can
-    user = models.ForeignKey(FergoUser, blank=False, null=True)
+    # TODO find a way to block editing this attribute (after it is created of course)
+    creator = models.ForeignKey(FergoUser, blank=False, null=True)
 
 
 class Task(models.Model):
-    # FIXME put a maxlength for the text field
-    title = models.CharField(max_length=settings.TASK_SETTINGS['name_max_length'], blank=False)
+    name = models.CharField(max_length=settings.TASK_SETTINGS['name_max_length'], blank=False)
     project = models.ForeignKey(Project, blank=False, null=True)
+
+
+class ProjectRelation(models.Model):
+    project = models.ForeignKey(Project, blank=False, null=True)
+    user = models.ForeignKey(FergoUser, blank=False, null=True)
+    relation_type = models.IntegerField(default=settings.PROJECT_RELATION['owned'])
+
+    class Meta:
+        unique_together = (("project", "user"),)
--- fergo/api_settings.py
+++ fergo/api_settings.py
@@ -1,8 +1,14 @@
 TASK_SETTINGS = {
-    'name_max_length': 100
+    'name_max_length': 200
 }
 
 
 PROJECT_SETTINGS = {
     'name_max_length': 20
+}
+
+PROJECT_RELATION = {
+    'owned': 1,
+    'shared_admin': 2,
+    'shared_readonly': 3
 }
